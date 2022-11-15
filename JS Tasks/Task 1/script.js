20abce0f9e8f8134909364315c7442e04523a6c4
Rimallasfcx111 <113599111+Rimallasfcx111@users.noreply.github.com>
2022-11-15 15:00:19 +0200

    Rimantas_Kazlauskas_js_exam

---
+++ JS Tasks/Task 1/script.js
@@ -0,0 +1,29 @@
+/* ------------------------------ TASK 1 ----------------------------
+Parašykite JS kodą, kuris leis vartotojui įvesti svorį kilogramais ir
+pamatyti jo pateikto svorio kovertavimą į:
+1. Svarus (lb) | Formulė: lb = kg * 2.2046
+2. Gramus (g) | Formulė: g = kg / 0.0010000
+3. Uncijos (oz) | Formulė: oz = kg * 35.274
+
+Pastaba: rezultatas turi būti matomas pateikus formą ir atvaizduojamas
+<div id="output"></div> viduje. Gautus atsakymus stilizuokite naudojant CSS;
+------------------------------------------------------------------- */
+function convertSvarai(num1) {
+    console.log(num1 * 2.2046);
+    
+}
+
+convertSvarai(10);
+
+function covertGramus(num1) {
+    console.log(num1 * 1000);
+
+}
+
+covertGramus(1);
+
+function convertUncijos(num1) {
+    console.log(num1 * 35.274);
+    
+}
+convertUncijos(1);
